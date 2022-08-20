e17c875e874a321e1954575223fa85b49653c4ee
jwilla <140156@unsaac.edu.pe>
2022-08-20 12:24:08 -0500

    component booking list

--- src/components/BookingItem.tsx
+++ src/components/BookingItem.tsx
@@ -1,12 +1,49 @@
-import { Text, View } from "react-native"
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native"
 import { colores } from "../theme/appTheme"
 
-export const BookingItem = () => {
+interface Props{
+  tour: string,
+  customerId: string,
+  customerName: string,
+  nTravelers?: number
+}
+
+export const BookingItem = ({tour, customerId, customerName, nTravelers}: Props) => {
   return (
-    <View>
-      <Text style={{color: colores.text}}>
-        Evento
-      </Text>
-    </View>
+    <TouchableOpacity
+      onPress={() => {}}
+    >
+      <View style={styles.container}>
+        <Text style={styles.tourText}> {tour} </Text>
+        <Text style={styles.customerText}>
+          <Text style={{fontWeight: '600', fontSize: 14}}>
+            {customerName}
+          </Text>
+          <Text style={{fontWeight: '400', fontSize: 12}}> 
+            {nTravelers && ' - '+nTravelers+' pasajeros'}
+          </Text>
+        </Text>
+      </View>
+    </TouchableOpacity>
   )
 }
+
+const styles = StyleSheet.create({
+  container:{
+    marginVertical: 4,
+    paddingVertical: 4,
+    borderBottomColor: colores.secondary,
+    borderBottomWidth: StyleSheet.hairlineWidth
+  },
+  text: {
+    color: colores.text
+  },
+  tourText:{
+    color: colores.secondary,
+    fontWeight: '500',
+    fontSize: 20
+  },
+  customerText:{
+    color: colores.text
+  }
+})
