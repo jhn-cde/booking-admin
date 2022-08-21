1e921874abc26a74b1aac08b5f166c90ed706967
jwilla <140156@unsaac.edu.pe>
2022-08-20 21:23:00 -0500

    using selector

--- src/components/BookingsList.tsx
+++ src/components/BookingsList.tsx
@@ -1,64 +1,24 @@
 import { useContext } from "react"
-import { View } from "react-native"
+import { ScrollView, View } from "react-native"
 import { DateContext } from "../context/DateContext"
 import { BookingItem } from "./BookingItem"
-
-//temporal
-const bookings = [
-  {
-    id: '123',
-    data: {
-      tour: 'Cusco',
-      customerId: 'customer1a',
-      customerName: 'Will A. M',
-      nTravelers: 4,
-      date: '2022/08/20'
-    }
-  },
-  {
-    id: '321',
-    data: {
-      tour: 'Machupicchu',
-      customerId: 'customer1b',
-      customerName: 'Elizabeth',
-      nTravelers: 3,
-      date: '2022/08/20'
-    }
-  },
-  {
-    id: '213',
-    data: {
-      tour: 'Manu',
-      customerId: 'customer1c',
-      customerName: 'Albus',
-      nTravelers: 1,
-      date: '2022/08/21'
-    }
-  },
-  {
-    id: '312',
-    data: {
-      tour: 'Manu',
-      customerId: 'customer1c',
-      customerName: 'Grind',
-      nTravelers: 1,
-      date: '2022/08/21'
-    }
-  },
-]
+import { getBookingsByTourDate } from "../selector/getBookingByTourDate"
+import { SafeAreaView } from "react-native-safe-area-context"
 
 export const BookingsList = () => {
   const { dateState } = useContext(DateContext)
+
+  const bookings = getBookingsByTourDate(dateState.curDate)
+
   return (
-    <View>
-      {
-        bookings
-        .filter((booking) =>
-          dateState.curDate.getDate() === new Date(booking.data.date).getDate()
-        ).map((booking) =>
-          <BookingItem key={booking.id} {...booking.data}/>
-        )
-      }
-    </View>
+    <SafeAreaView style={{flex:1}}>
+      <ScrollView>
+        {
+          bookings.map(
+            (booking) => <BookingItem key={booking.id} {...booking}/>
+          )
+        }
+      </ScrollView>
+    </SafeAreaView>
   )
 }
