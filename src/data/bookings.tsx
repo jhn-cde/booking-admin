0bc3c9ba3a605855b8905947ec28950862ad03d2
jwilla <140156@unsaac.edu.pe>
2022-08-20 20:09:36 -0500

    bookings example data

---
+++ src/data/bookings.tsx
@@ -0,0 +1,197 @@
+export const bookings = [
+  {
+    id: '1',
+    tour: 'Cusco',
+    nTravelers: 4,
+    bookingDate: '2022/05/2',
+    tourDate: '2022/08/20',
+    customer: {
+      doc: '1234',
+      name: 'Will A. M',
+      phone: '+19999192',
+      email: 'willam@gmail.com',
+    }
+  },
+  {
+    id: '2',
+    tour: 'Machupicchu',
+    nTravelers: 3,
+    bookingDate: '2022/06/20',
+    tourDate: '2022/08/20',
+    customer: {
+      doc: '1235',
+      name: 'Elizabeth',
+      phone: '+19999192',
+      email: 'willam@gmail.com',
+    }
+  },
+  {
+    id: '3',
+    tour: 'Manu',
+    nTravelers: 1,
+    bookingDate: '2022/06/20',
+    tourDate: '2022/08/21',
+    data: {
+      doc: '1236',
+      name: 'Albus',
+      phone: '+19999192',
+      email: 'willam@gmail.com',
+    }
+  },
+  {
+    id: '4',
+    tour: 'Manu',
+    nTravelers: 1,
+    bookingDate: '2022/06/20',
+    tourDate: '2022/08/21',
+    data: {
+      doc: '1237',
+      name: 'Grind',
+      phone: '+19999192',
+      email: 'willam@gmail.com',
+    }
+  },
+  {
+    id: '5',
+    tour: 'Manu',
+    nTravelers: 1,
+    bookingDate: '2022/06/20',
+    tourDate: '2022/08/21',
+    data: {
+      doc: '1238',
+      name: 'Newt',
+      phone: '+19999192',
+      email: 'newt@gmail.com',
+    }
+  },
+  {
+    id: '6',
+    tour: 'Humantay',
+    nTravelers: 2,
+    bookingDate: '2022/06/20',
+    tourDate: '2022/09/15',
+    data: {
+      doc: '1239',
+      name: 'Frodo',
+      phone: '+19999192',
+      email: 'bolson@gmail.com',
+    }
+  },
+  {
+    id: '7',
+    tour: 'Humantay',
+    nTravelers: 1,
+    bookingDate: '2022/06/20',
+    tourDate: '2022/09/15',
+    data: {
+      doc: '1240',
+      name: 'Smeagol',
+      phone: '+19999192',
+      email: 'miprecioso@gmail.com',
+    }
+  },
+  {
+    id: '8',
+    tour: 'Machupicchu',
+    nTravelers: 3,
+    bookingDate: '2022/06/20',
+    tourDate: '2022/09/20',
+    data: {
+      doc: '1241',
+      name: 'Aragorn',
+      phone: '+19999192',
+      email: 'araghunters@gmail.com',
+    }
+  },
+  {
+    id: '9',
+    tour: 'Cusco',
+    nTravelers: 1,
+    bookingDate: '2022/05/20',
+    tourDate: '2022/09/10',
+    data: {
+      doc: '1242',
+      name: 'Avicii',
+      phone: '+19999192',
+      email: 'avicii@gmail.com',
+    }
+  },
+  {
+    id: '10',
+    tour: 'Cusco',
+    nTravelers: 1,
+    bookingDate: '2022/05/21',
+    tourDate: '2022/09/11',
+    data: {
+      doc: '1243',
+      name: 'Mike',
+      phone: '+19999192',
+      email: 'posner@gmail.com',
+    }
+  },
+  {
+    id: '11',
+    tour: 'Tambopata',
+    nTravelers: 2,
+    bookingDate: '2022/07/12',
+    tourDate: '2022/10/01',
+    data: {
+      doc: '1244',
+      name: 'Martin',
+      phone: '+19999192',
+      email: 'garrix@gmail.com',
+    }
+  },
+  {
+    id: '12',
+    tour: 'Machupicchu',
+    nTravelers: 4,
+    bookingDate: '2022/07/10',
+    tourDate: '2022/10/12',
+    data: {
+      doc: '1245',
+      name: 'Sonny',
+      phone: '+19999192',
+      email: 'skrillex@gmail.com',
+    }
+  },
+  {
+    id: '13',
+    tour: 'Machupicchu',
+    nTravelers: 1,
+    bookingDate: '2022/07/28',
+    tourDate: '2022/11/01',
+    data: {
+      doc: '1246',
+      name: 'Martin',
+      phone: '+19999192',
+      email: 'garrix@gmail.com',
+    }
+  },
+  {
+    id: '14',
+    tour: 'Machupicchu',
+    nTravelers: 1,
+    bookingDate: '2022/06/28',
+    tourDate: '2022/11/01',
+    data: {
+      doc: '1247',
+      name: 'David',
+      phone: '+19999192',
+      email: 'gueta@gmail.com',
+    }
+  },
+  {
+    id: '15',
+    tour: 'Tambopata',
+    nTravelers: 1,
+    bookingDate: '2022/07/24',
+    tourDate: '2022/10/01',
+    data: {
+      doc: '1248',
+      name: 'Kygo',
+      phone: '+19999192',
+      email: 'kygo@gmail.com',
+    }
+  },
+]
