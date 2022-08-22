8fe1fc8b80f9476dfa0967c8b353bb05a3a9c2ae
jwilla <140156@unsaac.edu.pe>
2022-08-22 13:12:38 -0500

    using pressable

--- src/components/FloatingButton.tsx
+++ src/components/FloatingButton.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, TouchableNativeFeedback, View } from "react-native"
+import { Pressable, StyleSheet, TouchableNativeFeedback, View } from "react-native"
 import Icon from '@expo/vector-icons/MaterialCommunityIcons';
 import { colores, tema } from "../theme/appTheme";
 
@@ -16,14 +16,26 @@ export const FloatingButton = ({navigateTo}: Props) => {
     <View
       style={styles.botonContainer}
     >
-      <TouchableNativeFeedback
-        background={TouchableNativeFeedback.Ripple(colores.primary, false, 30)}
-        onPress={ navigate }
-      >
-        <View style={styles.boton}>
-          <Icon name="plus" color={'white'} size={35} />
-        </View>
-      </TouchableNativeFeedback>
+        <Pressable
+          style={({ pressed }) => [
+            {
+              backgroundColor: pressed
+              ? colores.acento2
+              : colores.acento
+            },
+            styles.boton
+          ]}
+          onPress={ navigate }
+        >
+          {({ pressed }) => (
+          <Icon
+            name="plus"
+            style={{
+              ...styles.text,
+            }}
+          />
+        )}
+        </Pressable>
     </View>
   )
 }
@@ -45,12 +57,10 @@ const styles = StyleSheet.create({
     borderRadius: 50,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: colores.acento,
-
-    shadowColor: tema == 'dia'? colores.text: colores.acento,
-    shadowOpacity: 0.4,
-    shadowRadius: 1,
-    shadowOffset: {width: 1, height: 3},
-    elevation: 8
+    elevation: 3
+  },
+  text:{
+    fontSize: 35,
+    color: colores.primary
   }
 })
