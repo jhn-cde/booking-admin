525f31698f6eddd94200503121b25d8d97d9ec1c
jwilla <140156@unsaac.edu.pe>
2022-08-22 16:05:23 -0500

    scrollview tours pendientes

--- src/components/CustomerItem.tsx
+++ src/components/CustomerItem.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native"
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native"
 import Icon from '@expo/vector-icons/Ionicons';
 
 import { customerDataInterface } from "../helpers/getCustomers"
@@ -26,7 +26,7 @@ export const CustomerItem = ({name, nDoc, phone, email, tours, navigateTo}: item
       <View style={{...customStyles.content, marginTop: 10,}}>
         <View>
           <Text style={styles.text}>Tours pendientes</Text>
-          <View style={{...customStyles.content, justifyContent: 'flex-start'}}>
+          <ScrollView horizontal={true}>
             {tours.filter(tour => tour.state==='Pendiente').map(tour => {
               return (
               <View key={tour.id} style={{marginRight: 10}}>
@@ -39,7 +39,7 @@ export const CustomerItem = ({name, nDoc, phone, email, tours, navigateTo}: item
                 </TouchableOpacity>
               </View>)
             })}
-          </View>
+          </ScrollView>
         </View>
         <View style={{marginRight: 5}}>
           <Text style={styles.text}>Total</Text>
